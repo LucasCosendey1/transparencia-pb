2929a4b279dc3d899350a27f9c6911bc1b946d18
Lucas Cosendey <lukecosendey@gmail.com>
2026-03-17 18:38:31 -0300

    vai lá

--- app/plano-de-governo/page.tsx
+++ app/plano-de-governo/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import PlanoGovernoLayout from '../../components/PlanoGovernoLayout'
 export default function Page() {
   return <PlanoGovernoLayout paginaId="plano-de-governo" titulo="Plano de Governo 2025–2028" breadcrumb="Plano de Governo" />
